75b3a258e77b1737cd4c3fb7f7f68c8ec09b6194
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:48:54 +0200

    change entire code to check if the checker is checking correctly

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-result = ""  # initialize empty variable for string
-for ascii in range(97, 123):  # 97 = 'a' and 122 = 'z'
-    if chr(ascii) not in ['q', 'e']:  # chr convert number to ASCII character
-        result += chr(ascii)  # add letter to final string
-
-print("{}".format(result), end="")  # end="" used to skip auto new-line
+for i in range(97, 123):  # 97 = 'a' and 122 = 'z'
+    if (i != 101) and (i != 113):
+        print("{:s}".format(chr(i)), end="")
+        # chr convert number to ASCII character
+        # end="" used to skip auto new-line
