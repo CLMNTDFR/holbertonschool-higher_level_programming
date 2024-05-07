65599f8f6f9817e57955c9dfabb35bf700f3fa94
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 16:02:54 +0200

    function that prints two digit, always differents

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            print("{:d}{:d}, ".format(i, j), end="")
+            if i == 8 and j == 9:
+                print("{:d}{:d}".format(i, j))
