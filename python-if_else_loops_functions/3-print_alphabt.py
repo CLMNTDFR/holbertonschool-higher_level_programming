2e1f6845839a76cbda7ce1997644dca076601c6c
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:07:52 +0200

    upgrade function to check all requierment of the task

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 result = ""  # initialize empty variable for string
-unavailable_letter = {"e", "q"}  # array
 
 for ascii in range(97, 123):  # 97 = 'a' and 122 = 'z'
-    letter = chr(ascii)  # chr convert number to ASCII character
-    if letter not in unavailable_letter:
-        result += letter  # add letter to final string
+    if chr(ascii) not in ['q', 'e']:  # chr convert number to ASCII character
+        result += chr(ascii)  # add letter to final string
 
 print("{}".format(result), end="")  # end="" used to skip auto new-line
