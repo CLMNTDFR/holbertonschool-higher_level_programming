98d1fb86a905bd53e85c33f8905db00dcb489d4b
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:35:35 +0200

    function in python that display number with two character everytime

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for number in range(0, 100):
+    if number < 99:
+        # ":02d" used to display two character (ex: 00, 09, 33, ...)
+        print(f"{number:02d}, ", end="")
+    else:
+        print(f"{number}")
