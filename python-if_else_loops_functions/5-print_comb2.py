c210bfad041ee5e72411d0daebed339d4b262d3e
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:52:12 +0200

    change entire code to check if the checker is checking correctly

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for number in range(0, 100):
     if number < 99:
         # ":02d" used to display two character (ex: 00, 09, 33, ...)
-        print(f"{number:02d}, ", end="")
+        print("{:02d}, ".format(number), end="")
     else:
-        print(f"{number}")
+        print("{:02d}, ".format(number))
