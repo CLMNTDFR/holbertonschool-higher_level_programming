d05d230bd63b12253af30ed1ffe3d828bbb4569e
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 13:43:52 +0200

    function in python that print alphabet with ASCII characters and skip auto new-line after execution

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+str = ""  # initialize empty variable for string
+for letter in range(97, 123):
+    str += chr(letter)  # chr convert number to ASCII character
+print("{}".format(str), end="")  # end="" used to skip auto new-line
