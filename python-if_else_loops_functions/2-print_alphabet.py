28c188eab053bfb44af0de5b4bb2a2bf8daa9f67
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:47:17 +0200

    change entire code to check if the checker is checking correctly

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = ""  # initialize empty variable for string
-for letter in range(97, 123):
-    str += chr(letter)  # chr convert number to ASCII character
-print("{}".format(str), end="")  # end="" used to skip auto new-line
+for i in range(97, 123):
+    print("{:s}".format(chr(i)), end="")
+    # chr convert number to ASCII character
+    # end="" used to skip auto new-line
