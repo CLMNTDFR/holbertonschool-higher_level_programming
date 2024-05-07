918554d0dd1b37a697a994fe0dc662272ce9d6d0
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 16:33:53 +0200

    function in python that convert lowercase characters to uppercase and print a string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for c in str:  # iterate index until end of string
         if (ord(c) >= 97) and (ord(c) <= 122):  # check if is lowercase ASCII
             c = chr(ord(c) - 32)  # convert to uppercase
-        print("{:s}.format(c)", end="")  # print and skip new line
+        print("{:s}".format(c), end="")  # print and skip new line
     print("")  # print new line
