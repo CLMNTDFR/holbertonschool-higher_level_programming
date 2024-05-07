64685d25b097244a8da54b22737886d932a9711d
CLMNTDFR <deferclement59@gmail.com>
2024-05-07 14:41:05 +0200

    test

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 result = ""  # initialize empty variable for string
-
 for ascii in range(97, 123):  # 97 = 'a' and 122 = 'z'
     if chr(ascii) not in ['q', 'e']:  # chr convert number to ASCII character
         result += chr(ascii)  # add letter to final string
