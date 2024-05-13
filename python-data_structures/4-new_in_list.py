715e5258e5056c1f33bb58b0bf72147182d8084a
CLMNTDFR <deferclement59@gmail.com>
2024-05-13 13:46:19 +0200

    function in python that replaces an element in THE COPY of a given list without modifying the original list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    #  Create a real copy of a list (not just reference)
+    new_list = my_list[:]
+
+    if idx >= 0 and idx < len(new_list):
+        new_list[idx] = element
+
+    return new_list
