e8b5a1da96448b38c24431ab0c910d6b4d06d3eb
CLMNTDFR <deferclement59@gmail.com>
2024-05-13 16:11:29 +0200

    function that delete an element at the idx position in a list, without pop()

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        my_list_list = my_list[:idx] + my_list[idx+1:]
-        return my_list
+    length = len(my_list)
 
-    else:
-        return my_list
+    if idx < 0 or idx >= length:
+        return (my_list)
+
+    del my_list[idx]
+
+    return (my_list)
