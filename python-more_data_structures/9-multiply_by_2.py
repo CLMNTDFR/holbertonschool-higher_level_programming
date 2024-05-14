48c86729bbcbe68104ee97e081dfa67e18b2f224
CLMNTDFR <deferclement59@gmail.com>
2024-05-14 12:24:59 +0200

    function in python that return a new dictionary with unchanged key but with each item multiplied by two

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_dico = a_dictionary.copy()
-
-    for key in new_dico:
-        new_dico[key] = new_dico[key * 2]
+    #  keep the key and multiplies the item by two
+    new_dico = {key: item * 2 for key, item in a_dictionary.items()}
 
     return new_dico
