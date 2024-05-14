e3af5371ecb9218a9991d01d84c21edbc5bdcf69
CLMNTDFR <deferclement59@gmail.com>
2024-05-14 12:03:38 +0200

    function in python that creates a new [value/key] if doesn't exist of replace if exist

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    #  creates a new [value/key] if doesn't exist of replace if exist
+    a_dictionary[key] = value
+    return a_dictionary
