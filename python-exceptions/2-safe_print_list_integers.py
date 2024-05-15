02d49fba9f74fd6be2be7f0db07831bb2e591978
CLMNTDFR <deferclement59@gmail.com>
2024-05-15 12:23:13 +0200

    Function that prints the first x elements of a list and only integers.
    - Prototype: def safe_print_list_integers(my_list=[], x=0):
    - my_list can contain any type (integer, string, etc.)
    _ All integers have to be printed on the same line followed by a new line - other type of value in the list must be skipped (in silence).
    - x represents the number of elements to access in my_list
    - x can be bigger than the length of my_list - if it’s the case, an exception is expected to occur
    - Returns the real number of integers printed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,14 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            #  Check if the element is an integer before printing
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                nb_printed += 1
-
-        except IndexError:
-            # Exit the loop if index is out of range
-            pass
+            print("{:d}".format(my_list[i]), end="")
+            nb_printed += 1
 
         except (TypeError, ValueError):
             #  Skip in silence if Type isn't an integer or
