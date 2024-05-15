ddc101ec9876bd9f50ed7ec85b28d5fab69dadd0
CLMNTDFR <deferclement59@gmail.com>
2024-05-15 14:02:52 +0200

    Function that divides element by element 2 lists.
    - Prototype: def list_division(my_list_1, my_list_2, list_length):
    - my_list_1 and my_list_2 can contain any type (integer, string, etc.)
    - list_length can be bigger than the length of both lists
    - Returns a new list (length = list_length) with all divisions
    - If 2 elements can’t be divided, the division result should be equal to 0
    - If an element is not an integer or float:
    print: wrong type
    - If the division can’t be done (/0):
    print: division by 0
    - If my_list_1 or my_list_2 is too short
    print: out of range

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -27,4 +27,4 @@ def list_division(my_list_1, my_list_2, list_length):
             #  Add the final result to the empty list 'new_list'
             new_list.append(result)
 
-        return new_list
+    return new_list
