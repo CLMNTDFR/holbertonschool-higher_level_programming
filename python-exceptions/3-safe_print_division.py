db35206505c77b25b19b318ce05539c6db0631f8
CLMNTDFR <deferclement59@gmail.com>
2024-05-15 13:30:37 +0200

    Function that divides 2 integers and prints the result.
    - Prototype: def safe_print_division(a, b):
    - - You can assume that a and b are integers
    - The result of the division should print on the finally: section preceded by Inside result:
    - Returns the value of the division, otherwise: None

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
     try:
         result = a / b
-
     except ZeroDivisionError:
-        #  Check if 'b' is zero
         result = None
-
     finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
-
-    return result
+        print("Inside result: {}".format(result))
+        return result
