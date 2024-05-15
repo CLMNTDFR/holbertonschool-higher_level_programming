b0b219271d7dfbc932b82d1840e21bb411aee922
CLMNTDFR <deferclement59@gmail.com>
2024-05-15 12:00:06 +0200

    Function that prints an integer with "{:d}".format().
    - Prototype: def safe_print_integer(value):
    value can be any type (integer, string, etc.)
    - The integer should be printed followed by a new line
    - Returns True if value has been correctly printed (it means the value is an integer)
    - Otherwise, returns False
    You have to use try: / except:
    You have to use "{:d}".format() to print as integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,11 +2,23 @@
 
 
 def safe_print_integer(value):
-    #  Try to print an integer and return 'True' if operation is successful
-    try:
-        print("{:d}".format(value))
-        return True
+    """
+    Attempts to print the provided value as an integer.
 
-    #  If 'value' is not an integer return 'False'
-    except:
+    Args:
+        value (any): The value to be printed as an integer.
+
+    Returns:
+        bool: True if the value was successfully printed as an integer, otherwise False.
+    """
+    try:
+        # Try to format and print the value as an integer
+        print("{:d}".format(value))
+        # If successful, return True
+        return True
+    except ValueError:
+        # Handle the case where the value cannot be formatted as an integer
+        return False
+    except TypeError:
+        # Handle the case where the type of the value is not suitable for integer formatting
         return False
