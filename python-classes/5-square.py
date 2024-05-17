438a3a2c173166c710f547c9419b1bd66e51b121
CLMNTDFR <deferclement59@gmail.com>
2024-05-17 11:05:49 +0200

    File who initializes a class Square that defines a square by: (based on 4-square.py)
    Private instance attribute: size:
    1) property def size(self): to retrieve it
    2) property setter def size(self, value): to set it:
    a) size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    b) if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with optional size: def __init__(self, size=0):
    3) Public instance method: def area(self): that returns the current square area
    4) Public instance method: def my_print(self): that prints in stdout the square with the character #:
    if size is equal to 0, print an empty line

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -56,7 +56,7 @@ class Square:
         """
         if self.__size == 0:
             print()
-        
+
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
