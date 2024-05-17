4704829ba815c07f18d1ba64b20fd101a3c40c67
CLMNTDFR <deferclement59@gmail.com>
2024-05-17 10:44:39 +0200

    File who initializes a class Square that defines a square by: (based on 3-square.py)
    Private instance attribute: size:
    1) property def size(self): to retrieve it
    2) property setter def size(self, value): to set it:
    a) size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    b) if size is less than 0, raise a ValueError exception with the message size must be >= 0
    Instantiation with optional size: def __init__(self, size=0):
    3) Public instance method: def area(self): that returns the current square area

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,19 +26,20 @@ class Square:
         """
         self.__size = size
 
-        @property
-        def size(self):
-            """Return the size of the square"""
-            return self.__size
+    @property
+    def size(self):
+        """Return the size of the square"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Check execption type and value for size"""
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
+    @size.setter
+    def size(self, value):
+        """Check execption type and value for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
 
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """
