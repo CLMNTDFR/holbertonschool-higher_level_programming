70ae40a5e4e407e2b355110b4702c21b39fc4cb7
CLMNTDFR <deferclement59@gmail.com>
2024-05-17 09:33:09 +0200

    File who contains an empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-square.py
+
+This module initializes an empty class called Square.
+"""
+
+
+class Square:
+    """
+    The 'Square' class represents a generic square.
+    """
