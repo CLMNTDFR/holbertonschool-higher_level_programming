63dff52e85f2e34c5e4709a95d4159e9669b4c9b
CLMNTDFR <deferclement59@gmail.com>
2024-05-20 13:08:23 +0200

    Function that prints My name is <first name> <last name>
    - Prototype: def say_my_name(first_name, last_name=""):
    - first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
    
    ________________
    
    Test file who cover many kinds of case like passing boolean, dictionary, tuple, list, integer, float, None, ...

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a function that prints a name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the first and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Returns:
+        None
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    # Print the name
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
