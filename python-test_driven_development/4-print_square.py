13e25ec5e902d1d2f5c9264ce51a38840c15544d
CLMNTDFR <deferclement59@gmail.com>
2024-05-20 16:53:25 +0200

    Function that prints a square with the character #.
    - Prototype: def print_square(size):
    size is the size length of the square
    - size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    - if size is less than 0, raise a ValueError exception with the message size must be >= 0
    - if size is a float and is less than 0, raise a TypeError exception with the message size must be an integer

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-This module print a square with the character #
+
+This module is composed by a function that prints a square with the character #
+
 """
 
 
 def print_square(size):
-    """Print a square with the character #
+    """Function that prints a square with the character #
 
     Args:
-        size (int): The size of the square
-
-    Raises:
-        TypeError: if size is not an integer
-        TypeError: if size is a float and less than 0
-        ValueError: if size is less than 0
+        size: size of the square printed
 
     Returns:
-        None
+        No return
+
+    Raises:
+        TypeError: If size is not an integer number
+
+
     """
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
