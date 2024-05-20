10ec72347530d9ac2404feda986fd0848961a00f
CLMNTDFR <deferclement59@gmail.com>
2024-05-20 19:44:48 +0200

    file script in python that creates an empty classe called "rectangle"

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-rectangle.py
+
+This module initializes an empty class called Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    The 'Rectangle' class represents a generic square.
+    """
