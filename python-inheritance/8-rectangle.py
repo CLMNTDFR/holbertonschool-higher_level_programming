5c729c163a015e17af999b794c582c428a32fd39
CLMNTDFR <deferclement59@gmail.com>
2024-05-22 14:57:57 +0200

    Add: a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
    1) Instantiation with width and height: def __init__(self, width, height):
    a) width and height must be private. No getter or setter
    b) width and height must be positive integers, validated by integer_validator

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,37 +4,12 @@
 """
 8-base_geometry.py
 
-This module initializes a class called BaseGeometry.
+This module initializes a subclass Rectangle
+(inherite from BaseGeometry)
 """
 
 
-class BaseGeometry:
-    """
-    The 'BaseGeometry' class.
-    """
-
-    def __init__(self):
-        """Initialize the class (even if empty)."""
-        pass
-
-    def area(self):
-        """
-        Public instance method: def area(self):
-        raises an Exception with the message 'area() is not implemented'.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method that validates value:
-        raise a TypeError if value is not an int
-        raise a ValueError if value <= 0
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
