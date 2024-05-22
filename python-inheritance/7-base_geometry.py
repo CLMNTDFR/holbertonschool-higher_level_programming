e7dc1967fbcdaebb927d247bfaf1a963055ec41e
CLMNTDFR <deferclement59@gmail.com>
2024-05-22 14:19:11 +0200

    Add: - Public instance method: def integer_validator(self, name, value): that validates value: - you can assume name is always a string - if value is not an integer: raise a TypeError exception, with the message <name> must be an integer - if value is less or equal to 0: raise a ValueError exception with the message <name> must be greater than 0

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -31,7 +31,7 @@ class BaseGeometry:
         raise a ValueError if value <= 0
         """
         if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
