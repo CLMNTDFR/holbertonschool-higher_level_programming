6529d632caf3515da1f1aff480580e68f3a7b76d
CLMNTDFR <deferclement59@gmail.com>
2024-05-22 11:38:57 +0200

    Script that initialize an empty class called 'BaseGeometry'

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py
+
+This module initializes an empty class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    The 'BaseGeometry' class.
+    """
