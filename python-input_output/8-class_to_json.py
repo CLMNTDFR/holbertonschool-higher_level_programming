5f951a895aa355b1a17bc1d653d64f3f1293cfb0
CLMNTDFR <deferclement59@gmail.com>
2024-05-27 15:16:58 +0200

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Script that converts an object's attributes to a dictionary
+suitable for JSON serialization.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary containing the attributes of obj.
+    """
+    json_obj = vars(obj)
+    return json_obj
