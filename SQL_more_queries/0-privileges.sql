9cff9fffb6e08d361a065712330d348777e7f1ef
CLMNTDFR <deferclement59@gmail.com>
2024-06-18 09:53:19 +0200

    script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Script to list all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
