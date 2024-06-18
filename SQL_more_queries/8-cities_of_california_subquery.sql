ad905234eed4bdd6a38be1677ac2dfc85ca2dddf
CLMNTDFR <deferclement59@gmail.com>
2024-06-18 10:38:44 +0200

    script that lists all the cities of California that can be found in the database hbtn_0d_usa.
    The states table contains only one record where name = California (but the id can be different, as per the example)
    Results must be sorted in ascending order by cities.id
    You are not allowed to use the JOIN keyword
    The database name will be passed as an argument of the mysql command

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,6 @@
 -- Select cities of California using subquery
 -- The state_id for California must be obtained from the states table
 -- The output should be ordered by the cities id
-SELECT * 
-FROM cities
-WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
+SELECT id, name FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = "California") 
 ORDER BY id ASC;
